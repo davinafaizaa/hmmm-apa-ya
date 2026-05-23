e27a8ca1f70d6a65ac6daaf041bf189160d3aa62
davinafaizaa <davinafaizaa@gmail.com>
2026-05-23 10:16:21 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
 #Hirarki teks
-st.title("Dashboard")
+st.title("📊 Dashboard")
 st.header("Laporan Bulanan")
 st.subheader("Monthly Expenses")
 st.write(" with love using streamlit"
