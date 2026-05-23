68b554d06a035b21aa91602df9fffdb55eccdccc
davinafaizaa <davinafaizaa@gmail.com>
2026-05-23 10:29:01 +0700

    Update app.py

--- app.py
+++ app.py
@@ -6,3 +6,5 @@ st.header("Laporan Bulanan")
 st.subheader("Monthly Expenses")
 st.write(" with love using streamlit"
         )
+title = st.text_input("Movie title", "Pride and Prejudice")
+st.write("The current movie title is", title)
